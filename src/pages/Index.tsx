624e5fe0116e025025be96b28bae5df0d2e8448c
Amrutha Satyamoorthy <121100404+amrutha2912@users.noreply.github.com>
2026-05-22 12:40:00 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,27 +1,27 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
-import About from "@/components/About";
+import BrandTeardowns from "@/components/BrandTeardowns";
+import DataProjects from "@/components/DataProjects";
 import Experience from "@/components/Experience";
-import Projects from "@/components/Projects";
-import Publications from "@/components/Publications";
+import About from "@/components/About";
+import Currently from "@/components/Currently";
 import Contact from "@/components/Contact";
-import ScrollProgress from "@/components/ScrollProgress";
 import BackToTop from "@/components/BackToTop";
-import BottomNavigation from "@/components/BottomNavigation";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background pb-20 md:pb-0">
-      <ScrollProgress />
+    <div className="min-h-screen bg-warm text-ink">
       <Navigation />
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Publications />
-      <Contact />
+      <main>
+        <Hero />
+        <BrandTeardowns />
+        <DataProjects />
+        <Experience />
+        <About />
+        <Currently />
+        <Contact />
+      </main>
       <BackToTop />
-      <BottomNavigation />
     </div>
   );
 };
